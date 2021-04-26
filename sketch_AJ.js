86d8ca63c67900acce1796537291b9d656b3adca
ambikajo <ambikajoshi@gmail.com>
2021-04-26 15:53:05 +0530

    some changes

--- sketch_AJ.js
+++ sketch_AJ.js
@@ -64,41 +64,9 @@ function setup(){
     palleteChanger.style("margin-right:30px")
 
     myPalette = [
-<<<<<<< HEAD
-        color('#ffffff'),
-        color('#0000ff'),
-        color('#3eb489'),
-        // color('#2d260c'),
-        // color('#451a0b'),
-        // color('#0b3b8d'),
-        // color('#171717'),
-        // color('#1e1e20'),
-        // color('#212025'),
-        // color('#2664c7'),
-        // color('#3b3a38'),
-        // color('#91cec9'),
-        // color('#9e9171'),
-        // color('#9fd4ca'),
-        // color('#b89a20'),
-        // color('#c70b23'),
-        // color('#cecdc9'),
-        // color('#d0b7ba'),
-        // color('#d6e8fc'),
-        // color('#d8d5d0'),
-        // color('#e86d1f'),
-        // color('#e8eeea'),
-        // color('#ec4942'),
-        // color('#f5e865'),
-        // color('#f697c3'),
-        // color('#fa79b9'),
-        // color('#fbe343'),
-        // color('#fc6320'),
-        // color('#fecb3c')
-=======
       color('#3eb489'),
       color('#ffffff'),
       color('#0000ff')
->>>>>>> 04aace8ebc0c4d323f4c549428add8f6ceb79500
     ];
 
 
@@ -155,11 +123,7 @@ function setup(){
     //     noiseCanvas.pixels[i*4+1] = c;
     //     noiseCanvas.pixels[i*4+2] = c;
     // }
-<<<<<<< HEAD
-    // noiseCanvas.updatePixels();
-=======
-    noiseCanvas.image(img, 0, 0, 64, 64)
->>>>>>> 04aace8ebc0c4d323f4c549428add8f6ceb79500
+    // noiseCanvas.image(img, 0, 0, 64, 64)
     let w = 0;
     let h = 5; 
     let offset = 13;
@@ -236,11 +200,8 @@ function inputVal(){
     }
   //   
   }
-<<<<<<< HEAD
 
 function frameStop(){
   state != state
   
 }
-=======
->>>>>>> 04aace8ebc0c4d323f4c549428add8f6ceb79500
